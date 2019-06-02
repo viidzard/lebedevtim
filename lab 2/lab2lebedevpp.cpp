7e776de90a57a1c274b04d4f3ebc7aa5d4559cb0
viidzard <viidzard@gmaill.com>
2019-06-02 18:10:37 +0900

    add lab2

---
+++ lab 2/lab2lebedevpp.cpp
@@ -0,0 +1,11 @@
+﻿
+#include "pch.h"
+#include "complex.h"
+
+int main()
+{
+	std::vector<complex> complexVector;
+	read_complex(complexVector);
+	write_complex(complexVector);
+	system("pause");
+}
