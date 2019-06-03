b17117e9a132d804c14ed1c53f89c92932d45596
viidzard <viidzard@gmaill.com>
2019-06-03 12:47:17 +0900

    add lab 3.4

---
+++ lab 3 zadanie 4/lab 3 zadanie 4/lab 3 zadanie 4.cpp
@@ -0,0 +1,13 @@
+
+
+#include "stdafx.h"
+#include <iostream>
+#include "myread.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a;
+	myread_num(a);
+	std::cout <<"\n"<< a;
+}
+
---
+++ lab 3 zadanie 4/lab 3 zadanie 4/myread.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+extern "C" void myread_num(int & c);
